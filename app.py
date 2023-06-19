2f173465d85acd9ba388343de7828c0df7f14ccf
BeeTwenty <43971937+BeeTwenty@users.noreply.github.com>
2023-06-19 06:01:01 +0200

    error handling

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ import crypt
 from functools import wraps
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_bootstrap import Bootstrap
+
 logger = logging.getLogger(__name__)
-
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -20,6 +19,23 @@ logging.basicConfig(level=logging.DEBUG)
 # Path to the Fail2ban configuration file
 FAIL2BAN_CONFIG_FILE = '/etc/fail2ban/jail.conf'
 
+def handle_exceptions(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            logger.exception(f"Error occurred in {func.__name__}: {str(e)}")
+            return render_template('error.html', message=str(e))
+        except subprocess.CalledProcessError as e:
+            logger.exception(f"Error occurred in {func.__name__}: {e.stderr}")
+            return render_template('error.html', message=e.stderr)
+        except Exception as e:
+            logger.exception(f"Error occurred in {func.__name__}: {str(e)}")
+            return render_template('error.html', message='An unexpected error occurred')
+    return wrapper
+
+@handle_exceptions
 def get_country(ip):
     reader = geoip2.database.Reader('/var/www/fail2ban-webui/GeoLite2-Country/GeoLite2-Country.mmdb')
     try:
@@ -29,7 +45,7 @@ def get_country(ip):
         country = 'Unknown'
     return country
 
-
+@handle_exceptions
 def read_fail2ban_config():
     """
     Read the Fail2ban configuration file and extract the relevant settings.
@@ -51,6 +67,7 @@ def read_fail2ban_config():
 
     return config
 
+@handle_exceptions
 def write_fail2ban_config(config):
     """
     Write the modified Fail2ban configuration back to the file.
@@ -71,13 +88,14 @@ def write_fail2ban_config(config):
             else:
                 file.write(line)
 
+@handle_exceptions
 def reload_fail2ban():
     """
     Execute the Fail2ban reload command.
     """
     subprocess.run(['fail2ban-client', 'reload'], capture_output=True, text=True)
 
-
+@handle_exceptions
 def get_banned_ips():
     command = ['fail2ban-client', 'status', 'sshd']
     result = subprocess.run(command, capture_output=True, text=True)
@@ -87,8 +105,7 @@ def get_banned_ips():
 
     return banned_ips 
 
-
-
+@handle_exceptions
 def add_banned_ip(ip):
     if not re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', ip):
         raise ValueError("Invalid IP address format")
@@ -96,8 +113,7 @@ def add_banned_ip(ip):
     command = ['fail2ban-client', 'set', 'sshd', 'banip', ip]
     subprocess.run(command, capture_output=True, text=True)
 
-    
-
+@handle_exceptions
 def delete_banned_ip(ip):
     if not re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', ip):
         raise ValueError("Invalid IP address format")
@@ -105,10 +121,7 @@ def delete_banned_ip(ip):
     command = ['fail2ban-client', 'set', 'sshd', 'unbanip', ip]
     subprocess.run(command, capture_output=True, text=True)
 
-
-
-
-
+@handle_exceptions
 def authenticate_system(username, password):
     try:
         # Retrieve the user's encrypted password from the system password database
@@ -117,29 +130,22 @@ def authenticate_system(username, password):
         # Verify the provided password against the encrypted password
         if crypt.crypt(password, encrypted_password) == encrypted_password:
             return True
-            session 
         else:
             return False
     except KeyError:
         return False
 
-
-
-
 def authenticate(username, password):
     # You can add additional authentication methods here
     if authenticate_system(username, password):
         return True
-        print("true")
     else:
         return False
-        print("false")
-
 
 def system_login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if  not authenticate_system(session['username'], session['password']):
+        if not authenticate_system(session.get('username'), session.get('password')):
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
@@ -159,53 +165,45 @@ def index():
 
     return render_template('index.html', banned_ips=banned_ips, top_countries=sorted_countries)
 
-
-
-
 @app.route('/config', methods=['GET', 'POST'])
 @system_login_required
 def config():
-    
-        if request.method == 'POST':
-            # Retrieve form data and update the configuration
-            maxretry = request.form.get('maxretry')
-            bantime = request.form.get('bantime')
-            findtime = request.form.get('findtime')
+    if request.method == 'POST':
+        # Retrieve form data and update the configuration
+        maxretry = request.form.get('maxretry')
+        bantime = request.form.get('bantime')
+        findtime = request.form.get('findtime')
 
-            config = read_fail2ban_config()
-            config['maxretry'] = maxretry
-            config['bantime'] = bantime
-            config['findtime'] = findtime
-            write_fail2ban_config(config)
+        config = read_fail2ban_config()
+        config['maxretry'] = maxretry
+        config['bantime'] = bantime
+        config['findtime'] = findtime
+        write_fail2ban_config(config)
 
-            # Reload Fail2ban
-            reload_fail2ban()
+        # Reload Fail2ban
+        reload_fail2ban()
 
-            # Redirect to the configuration page after saving
-            return redirect(url_for('config'))
-        else:
-            # Read the current Fail2ban configuration
-            config = read_fail2ban_config()
-            banned_ips = get_banned_ips()
-            return render_template('config.html', config=config, banned_ips=banned_ips) 
-    
+        # Redirect to the configuration page after saving
+        return redirect(url_for('config'))
+    else:
+        # Read the current Fail2ban configuration
+        config = read_fail2ban_config()
+        banned_ips = get_banned_ips()
+        return render_template('config.html', config=config, banned_ips=banned_ips) 
+
 @app.route('/banip', methods=['POST'])
 @system_login_required
 def ban_ip():
-    
-        ip = request.form.get('ip')
-        add_banned_ip(ip)
-        return redirect(url_for('config'))
-    
+    ip = request.form.get('ip')
+    add_banned_ip(ip)
+    return redirect(url_for('config'))
 
 @app.route('/unbanip', methods=['POST'])
 @system_login_required
 def unban_ip():
-    
-        ip = request.form.get('ip')
-        delete_banned_ip(ip)
-        return redirect(url_for('config'))
-   
+    ip = request.form.get('ip')
+    delete_banned_ip(ip)
+    return redirect(url_for('config'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
